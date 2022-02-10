d2d96659990347873141c71907de69f11d8960ea
Iskender TOTOĞLU <totoglu@gmail.com>
2022-02-10 18:14:46 +0300

    Add support for Turkish (tr) translation (#3)

---
+++ resources/lang/tr/health.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+
+    'pages' => [
+        'health_check_results' => [
+            'buttons' => [
+                'refresh' => 'Yenile',
+            ],
+
+            'heading' => 'Uygulama Sağlığı',
+
+            'navigation' => [
+                'group' => 'Ayarlar',
+                'label' => 'Uygulama Sağlığı',
+            ],
+
+            'notifications' => [
+                'check_results' => 'Sonuçları kontrol et.',
+            ],
+        ],
+    ],
+
+];
