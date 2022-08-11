d72bbb4de07dc828e38e94653621b0261d47ca2e
Ngô Quốc Đạt <56961917+datlechin@users.noreply.github.com>
2022-08-11 11:05:40 +0700

    Vietnamese translations

---
+++ resources/lang/vi/health.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+
+    'pages' => [
+        'health_check_results' => [
+            'buttons' => [
+                'refresh' => 'Làm mới',
+            ],
+            
+            'heading' => 'Tình trạng ứng dụng',
+            
+            'navigation' => [
+                'group' => 'Cài đặt',
+                'label' => 'Tình trạng ứng dụng',
+            ],
+
+            'notifications' => [
+                'check_results' => 'Kết quả được kiểm tra từ',
+            ],
+        ],
+    ],
+
+];
