10a087eb640d31bb46763b2b229a0559d1ce31fd
Flavio Medeiros <smedeiros.flavio@gmail.com>
2022-11-24 19:11:08 -0300

    Added pt_BR lang files

---
+++ resources/lang/pt_BR/health.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+
+    'pages' => [
+        'health_check_results' => [
+            'buttons' => [
+                'refresh' => 'Atualizar',
+            ],
+
+            'heading' => 'Saúde do aplicativo',
+
+            'navigation' => [
+                'group' => 'Configurações',
+                'label' => 'Saúde do aplicativo',
+            ],
+
+            'notifications' => [
+                'check_results' => 'Confira os resultados de',
+            ],
+        ],
+    ],
+
+];
