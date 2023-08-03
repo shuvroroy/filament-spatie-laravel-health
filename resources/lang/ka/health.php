09ead6a9f61bb40c3f91b8adbadaf42edc5a635d
Nick Gejadze <gejadze@gmail.com>
2023-08-03 09:04:13 +0400

    Georgian Transaltion (#33)

---
+++ resources/lang/ka/health.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+
+    'pages' => [
+        'health_check_results' => [
+            'buttons' => [
+                'refresh' => 'განახლება',
+            ],
+
+            'heading' => 'აპლიკაციის სტატუსი',
+
+            'navigation' => [
+                'group' => 'პარამეტრები',
+                'label' => 'სერვერის ჯანმრთელობა',
+            ],
+
+            'notifications' => [
+                'check_results' => 'ბოლოს შემოწმდა:',
+            ],
+        ],
+    ],
+
+];
