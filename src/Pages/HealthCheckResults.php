6807afe2b07861b0d532dd04f7b416817b851fc3
Ihsan Devs <ihsandevs@ihsandevs.com>
2023-08-06 02:13:47 +0700

    Update HealthCheckResults.php
    Fixing an error when calling the static method HealthCheckResults::make() during plugin registration.

--- src/Pages/HealthCheckResults.php
+++ src/Pages/HealthCheckResults.php
@@ -10,9 +10,14 @@ use Illuminate\Contracts\Support\Htmlable;
 use Illuminate\Support\Facades\Artisan;
 use Spatie\Health\Commands\RunHealthChecksCommand;
 use Spatie\Health\ResultStores\ResultStore;
+use Filament\Contracts\Plugin;
 
-class HealthCheckResults extends Page
+class HealthCheckResults extends Page implements Plugin
 {
+    public static function make() : static
+    {
+        return app( static::class);
+    }
     /**
      * @var array<string, string>
      */
@@ -22,49 +27,71 @@ class HealthCheckResults extends Page
 
     protected static string $view = 'filament-spatie-health::pages.health-check-results';
 
-    protected function getActions(): array
+    protected function getActions() : array
     {
         return [
-            Action::make(__('filament-spatie-health::health.pages.health_check_results.buttons.refresh'))
+            Action::make( __( 'filament-spatie-health::health.pages.health_check_results.buttons.refresh' ) )
                 ->button()
-                ->action('refresh'),
+                ->action( 'refresh' ),
         ];
     }
 
-    public function getHeading(): string | Htmlable
+    public function getHeading() : string|Htmlable
     {
-        return __('filament-spatie-health::health.pages.health_check_results.heading');
+        return __( 'filament-spatie-health::health.pages.health_check_results.heading' );
     }
 
-    public static function getNavigationGroup(): ?string
+    public static function getNavigationGroup() : ?string
     {
-        return __('filament-spatie-health::health.pages.health_check_results.navigation.group');
+        return __( 'filament-spatie-health::health.pages.health_check_results.navigation.group' );
     }
 
-    public static function getNavigationLabel(): string
+    public static function getNavigationLabel() : string
     {
-        return __('filament-spatie-health::health.pages.health_check_results.navigation.label');
+        return __( 'filament-spatie-health::health.pages.health_check_results.navigation.label' );
     }
 
-    protected function getViewData(): array
+    protected function getViewData() : array
     {
-        $checkResults = app(ResultStore::class)->latestResults();
+        $checkResults = app( ResultStore::class)->latestResults();
 
         return [
-            'lastRanAt' => new Carbon($checkResults?->finishedAt),
+            'lastRanAt'    => new Carbon( $checkResults?->finishedAt ),
             'checkResults' => $checkResults,
         ];
     }
 
-    public function refresh(): void
+    public function refresh() : void
     {
-        Artisan::call(RunHealthChecksCommand::class);
+        Artisan::call( RunHealthChecksCommand::class);
 
-        $this->dispatch('refresh-component');
+        $this->dispatch( 'refresh-component' );
 
         Notification::make()
-            ->title('Health check results refreshed')
+            ->title( 'Health check results refreshed' )
             ->success()
             ->send();
+    }
+    /**
+     * @param \Filament\Panel $panel
+     */
+    public function register(\Filament\Panel $panel) : void
+    {
+        $panel->pages( [
+            static::class,
+        ] );
+    }
+
+    /**
+     *
+     * @param \Filament\Panel $panel
+     */
+    public function boot(\Filament\Panel $panel) : void
+    {
+    }
+
+    public function getId() : string
+    {
+        return 'filament-spatie-health';
     }
 }
