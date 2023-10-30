d78d07cdaa4c40741f0a8186929086b3017f682a
fiere210 <fcontreras.andia@gmail.com>
2023-10-30 12:48:46 -0300

    Spanish lang (#40)
    Adds Spanish language

---
+++ resources/lang/es/health.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+
+    'pages' => [
+        'health_check_results' => [
+            'buttons' => [
+                'refresh' => 'Refrescar',
+            ],
+
+            'heading' => 'Salud de la aplicación',
+
+            'navigation' => [
+                'group' => 'Configuración',
+                'label' => 'Salud de la aplicación',
+            ],
+
+            'notifications' => [
+                'check_results' => 'Revisar resultados desde',
+            ],
+        ],
+    ],
+    
+];
