a6cbac918733c9f0c85eb91284cd6980d0e5de17
becker <beckermaxime84@gmail.com>
2024-02-09 14:43:44 +0100

    add lang pt_PT

---
+++ resources/lang/pt_PT/health.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+
+    'pages' => [
+        'health_check_results' => [
+            'buttons' => [
+                'refresh' => 'Atualizar',
+            ],
+
+            'heading' => 'Saúde da Aplicação',
+
+            'navigation' => [
+                'group' => 'Configurações',
+                'label' => 'Saúde da Aplicação',
+            ],
+
+            'notifications' => [
+                'check_results' => 'Verificar resultados de',
+            ],
+        ],
+    ],
+
+];
