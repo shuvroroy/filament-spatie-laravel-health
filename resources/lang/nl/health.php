e586dbe18c6a099948927f50a648a37f401f1a96
becker <beckermaxime84@gmail.com>
2024-02-09 11:54:06 +0100

    add lang nl

---
+++ resources/lang/nl/health.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+
+    'pages' => [
+        'health_check_results' => [
+            'buttons' => [
+                'refresh' => 'Vernieuwen',
+            ],
+
+            'heading' => 'Gezondheid van de applicatie',
+
+            'navigation' => [
+                'group' => 'Instellingen',
+                'label' => 'Gezondheid van de applicatie',
+            ],
+
+            'notifications' => [
+                'check_results' => 'Controleer resultaten van',
+            ],
+        ],
+    ],
+
+];
