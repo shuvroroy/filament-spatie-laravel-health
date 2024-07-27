12add5e6d1d9f36f52047453a44f8c477ff337c3
Ján Hamrák <snickom@gmail.com>
2024-07-27 16:56:18 +0200

    Slovak translation (#50)

---
+++ resources/lang/sk/health.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+
+    'pages' => [
+        'health_check_results' => [
+            'buttons' => [
+                'refresh' => 'Obnoviť',
+            ],
+
+            'heading' => 'Stav aplikácie',
+
+            'navigation' => [
+                'group' => 'Nastavenia',
+                'label' => 'Stav aplikácie',
+            ],
+
+            'notifications' => [
+                'check_results' => 'Skontrolovať výsledky od',
+            ],
+        ],
+    ],
+
+];
