cae6acaa9e068dcb70933802fb4ec1f35f66f385
Wildan <cuinc99@gmail.com>
2025-01-12 00:14:40 +0800

    feat: add Indonesian language support for health check results (#66)

---
+++ resources/lang/id/health.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+
+    'pages' => [
+        'health_check_results' => [
+            'buttons' => [
+                'refresh' => 'Segarkan',
+            ],
+
+            'heading' => 'Kesehatan Aplikasi',
+
+            'navigation' => [
+                'group' => 'Pengaturan',
+                'label' => 'Kesehatan Aplikasi',
+            ],
+
+            'notifications' => [
+                'check_results' => 'Periksa hasil dari :lastRanAt',
+                'results_refreshed' => 'Hasil pemeriksaan kesehatan disegarkan',
+            ],
+        ],
+    ],
+
+];
