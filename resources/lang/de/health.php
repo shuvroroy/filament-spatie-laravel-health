b96b93b3496096bba16c3b3191b8562194954c68
grafst <grafst@users.noreply.github.com>
2025-09-29 15:12:28 +0200

    add german translation (#86)

---
+++ resources/lang/de/health.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+
+    'pages' => [
+        'health_check_results' => [
+            'buttons' => [
+                'refresh' => 'Aktualisieren',
+            ],
+
+            'heading' => 'Anwendungsgesundheit',
+
+            'navigation' => [
+                'group' => 'Einstellungen',
+                'label' => 'Anwendungsgesundheit',
+            ],
+
+            'notifications' => [
+                'check_results' => 'Geprüft: :lastRanAt',
+                'results_refreshed' => 'Prüfergebnisse wurden aktualisiert',
+            ],
+        ],
+    ],
+
+];
