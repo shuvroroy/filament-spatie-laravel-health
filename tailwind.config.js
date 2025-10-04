bcd0f1eeee22349b434be306da7745292f663968
Shuvro Roy <shuvro.nsu.cse@gmail.com>
2025-10-04 21:53:47 +0600

    chore: clean up

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    content: ["./resources/views/**/*.blade.php"],
-    darkMode: "class",
-    important: ".filament-spatie-health",
-    plugins: [],
-    corePlugins: {
-      preflight: false,
-    }
-};
