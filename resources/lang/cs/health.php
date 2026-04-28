8ace7efe0bfa755f9eb0c216fb4300559638e7cc
Th3JK <97049204+Th3JK@users.noreply.github.com>
2026-04-28 16:24:48 +0200

    Created czech translation.

---
+++ resources/lang/cs/health.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+
+    'pages' => [
+        'health_check_results' => [
+            'buttons' => [
+                'refresh' => 'Obnovit',
+            ],
+
+            'heading' => 'Stav aplikace',
+
+            'navigation' => [
+                'group' => 'Nastavení',
+                'label' => 'Stav aplikace',
+            ],
+
+            'notifications' => [
+                'check_results' => 'Výsledky kontroly z :lastRanAt',
+                'results_refreshed' => 'Výsledky kontroly stavu byly aktualizovány',
+            ],
+        ],
+    ],
+
+];
